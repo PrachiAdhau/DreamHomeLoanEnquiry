dda9fe1fa8d114f3236085e2b6b1d018b1d17d82
priyankagodse007 <priyankagodse007@gmail.com>
2024-10-10 21:02:47 +0530

    Merge branch 'master' of https://github.com/PrachiAdhau/DreamHomeLoanEnquiry.git

--- DreamHousingLoan/src/main/java/com/iptech/dreamhousingloan/controller/EnquiryController.java
+++ DreamHousingLoan/src/main/java/com/iptech/dreamhousingloan/controller/EnquiryController.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.iptech.dreamhousingloan.model.Enquiry;
@@ -35,6 +38,16 @@ public class EnquiryController {
 		List<Enquiry> list = sei.getAllData();
 		return new ResponseEntity<List<Enquiry>>(list,HttpStatus.OK);
 	}
-
-
+//@PutMapping("/updatedata/{applicant_Id}")
+//public ResponseEntity<Enquiry> updatedata(@RequestPart ("applicant") String  applicant_Id)
+//{
+	
+	
+	
+	
+	
+	
+	
 }
+
+

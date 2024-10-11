e83ff904063ae22646fe46371d2db9a7c9d4af83
ramnath98 <dongareramnath55@gmail.com>
2024-10-11 14:01:50 -0400

    remove package

--- DreamHousingLoan/src/main/java/com/iptech/dreamhousingloan/serviceimpl/ServiceEnquiryImpl.java
+++ DreamHousingLoan/src/main/java/com/iptech/dreamhousingloan/serviceimpl/ServiceEnquiryImpl.java
@@ -5,8 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+
 import org.springframework.stereotype.Service;
 
 import com.iptech.dreamhousingloan.model.Enquiry;

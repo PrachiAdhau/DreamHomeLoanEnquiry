100e9a979180eb1e62ed79d75bbe9d8a38b7626f
2507praju <prajktaghodeswar@gmail.com>
2024-10-14 16:56:25 +0530

    added

--- DreamHousingLoan/src/main/java/com/iptech/dreamhousingloan/serviceimpl/ServiceEnquiryImpl.java
+++ DreamHousingLoan/src/main/java/com/iptech/dreamhousingloan/serviceimpl/ServiceEnquiryImpl.java
@@ -7,9 +7,9 @@ import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
 
 import org.springframework.stereotype.Service;
 
